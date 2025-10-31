860dff58d847c1ae8f231d58843ef11f9a557531
AM-Tower <occupant.am.tower@gmail.com>
2025-10-31 13:46:44 -0700

    Add .gitignore and remove build artifacts

--- build/Desktop_Qt_6_10_0_llvm_mingw_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_PipMatrixResolverQt_FILE C:/Users/jflesher/OneDrive/Documents/PipMatrixResolver/Qt/build/Desktop_Qt_6_10_0_llvm_mingw_64_bit-Debug/PipMatrixResolverQt.exe)
-set(__QT_DEPLOY_TARGET_PipMatrixResolverQt_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_PipMatrixResolverQt_RUNTIME_DLLS C:/Qt/6.10.0/llvm-mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.0/llvm-mingw_64/bin/Qt6Network.dll;C:/Qt/6.10.0/llvm-mingw_64/bin/Qt6Concurrent.dll;C:/Qt/6.10.0/llvm-mingw_64/bin/Qt6Svg.dll;C:/Qt/6.10.0/llvm-mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.0/llvm-mingw_64/bin/Qt6Core.dll)
